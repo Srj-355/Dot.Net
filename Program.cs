82cbbbb93a1eb14b2e29cc9c60c3b520f15d7fa3
Dragon03 <saroj80977@gmail.com>
2024-07-20 12:18:19 +0545

    rrr

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ public class Program
            li.Add(new Movie("Pathan"));
            li.Add(new Movie("sahoo"));
            li.Add(new Movie("KGF 2"));
+           li.Add(new Movie("RRR"));
            foreach(var movie in li){
             Console.WriteLine(movie.getTitle());
            }
